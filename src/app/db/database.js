ca32ef56fc2000a305c83bcc34e18101ee0eff20
Henry <henry.karppinen1997@gmail.com>
2026-09-26 22:28:10 +0300

    Added reusable mongodb connection, added dependency

---
+++ src/app/db/database.js
@@ -0,0 +1,19 @@
+import { MongoClient } from 'mongodb';
+import { uri } from '../passwd';
+
+const client = new MongoClient(uri);
+
+export async function connectToMongoDB() {
+  try {
+    await client.connect();
+    console.log("You successfully connected to MongoDB!");
+    return client;
+  } catch (err) {
+    console.dir(err);
+  }
+}
+
+// Call this only when your application terminates
+export async function disconnectFromMongoDB() {
+  await client.close();
+}
